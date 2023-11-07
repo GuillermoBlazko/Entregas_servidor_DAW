12f8fd38a4ed014594644ba40c58706212bd1bca
Guillermo Vaello <131365230+GuillermoBlazko@users.noreply.github.com>
2023-11-07 17:51:03 +0100

    Update jugar.php

--- casino/jugar.php
+++ casino/jugar.php
@@ -13,7 +13,7 @@
     <p><?=$msg?></p>
     <form action="<?=$_SERVER['PHP_SELF'];?>" method="post">
         <label for="dineroapuesta">Introduca el dinero que va a jugar: </label><input type="number" name="dineroApuesta" id="" min=0><br><br>
-        <label for="">Tipo de apuesta: </label><input type="radio" name="apuesta" id="" value="par">Par <input type="radio" name="apuesta" id="" value="Impar">Impar <br>
+        <label for="">Tipo de apuesta: </label><input type="radio" name="apuesta" id="" value="Par">Par <input type="radio" name="apuesta" id="" value="Impar">Impar <br>
         <input type="submit" name="accion" value="Apostar"><input type="submit" name="accion" value="Añadir"><input type="submit" name="accion" value="Abandonar">
     </form>
 </body>
