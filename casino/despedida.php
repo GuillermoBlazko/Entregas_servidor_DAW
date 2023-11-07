01924cae2877c8ca4e073b94b95f6fc70d3fa53f
Guillermo Vaello <131365230+GuillermoBlazko@users.noreply.github.com>
2023-11-07 12:38:07 +0100

    Add files via upload

---
+++ casino/despedida.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>despedida</title>
+</head>
+<body>
+    <p>muchas gracias por juar con nosotros</p>
+    <p>Su resultado final es de <?=$_SESSION["cantidad"]?> Euros</p>
+</body>
+</html>
