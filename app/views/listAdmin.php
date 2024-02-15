2a40224053651ab7708e3875809765443154dd36
Guillermo Vaello <131365230+GuillermoBlazko@users.noreply.github.com>
2024-02-15 16:11:37 +0100

    Add files via upload

---
+++ app/views/listAdmin.php
@@ -0,0 +1,65 @@
+<form>
+    <button type="submit" name="orden" value="Nuevo"> Cliente Nuevo </button><br>
+</form>
+<br>
+
+<?php
+if(isset($_SESSION["ordenAD"])){
+    $ordenAD = $_SESSION["ordenAD"]; 
+}
+
+
+?>
+
+<!-- OLD: FORM: Tipo de ordenación --> 
+<!-- <form action="">
+    orden: 
+    <select name="ordenacion" id="orden">
+        <option value="id">id</option>
+        <option value="first_name">First_name</option>
+        <option value="email">email</option>
+        <option value="gender">gender</option>
+        <option value="ip_address">ip_address</option>
+        <option value="telefono">telefono</option>
+
+    </select>
+    <input type="submit" value=" aceptar ">
+</form> -->
+<br>
+<table>
+    <tr>
+        <th><a href="?ordenacion=id">id</a> <?=($ordenAD == "ASC")?"▲":"▼"?></th>
+        <th><a href="?ordenacion=first_name">first_name</a> <?=($ordenAD == "ASC")?"▲":"▼"?></th>
+        <th><a href="?ordenacion=email">email</a> <?=($ordenAD == "ASC")?"▲":"▼"?></th>
+        <th><a href="?ordenacion=gender">gender</a> <?=($ordenAD == "ASC")?"▲":"▼"?>
+        <th><a href="?ordenacion=ip_address">ip_address</a> <?=($ordenAD == "ASC")?"▲":"▼"?>
+        <th><a href="?ordenacion=telefono">teléfono</a> <?=($ordenAD == "ASC")?"▲":"▼"?>
+    </tr>
+    <?php foreach ($tvalores as $valor) : ?>
+        <tr>
+            <td><?= $valor->id ?> </td>
+            <td><?= $valor->first_name ?> </td>
+            <td><?= $valor->email ?> </td>
+            <td><?= $valor->gender ?> </td>
+            <td><?= $valor->ip_address ?> </td>
+            <td><?= $valor->telefono ?> </td>
+            <td><a href="#" onclick="confirmarBorrar('<?= $valor->first_name ?>',<?= $valor->id ?>);">Borrar</a></td>
+            <td><a href="?orden=Modificar&id=<?= $valor->id ?>">Modificar</a></td>
+            <td><a href="?orden=Detalles&id=<?= $valor->id ?>">Detalles</a></td>
+
+        <tr>
+        <?php endforeach ?>
+</table>
+
+<form>
+    <br>
+    <button type="submit" name="nav" value="Primero">
+        << </button>
+            <button type="submit" name="nav" value="Anterior">
+                < </button>
+                    <button type="submit" name="nav" value="Siguiente"> > </button>
+                    <button type="submit" name="nav" value="Ultimo"> >> </button>
+</form>
+<form action="" style="text-align: right;">
+    <button type="submit" name="orden" value="Terminar">Terminar</button>
+</form>
